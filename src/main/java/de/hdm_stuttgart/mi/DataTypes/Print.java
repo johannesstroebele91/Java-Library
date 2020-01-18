60def864e64db92f5cde848418e5f487297d6da4
JohannesStroebele <stroebele.j@gmail.com>
2020-01-18 14:00:43 +0100

    Added knowledge for testing in Java

--- src/main/java/de/hdm_stuttgart/mi/DataTypes/Print.java
+++ src/main/java/de/hdm_stuttgart/mi/DataTypes/Print.java
@@ -1,16 +1,16 @@
-package de.hdm_stuttgart.mi;
+package de.hdm_stuttgart.mi.DataTypes;
 
 public class Print {
-    static public void print() {             // (void)
+    static private void print() {             // (void)
         System.out.println("No argument");
     }
-    static public void print(int i) {        // (int)
+    static private void print(int i) {        // (int)
         System.out.println("int value " + i);
     }
-    static public void print(double d) {     // (double)
+    static private void print(double d) {     // (double)
         System.out.println("double value " + d);
     }
-    static public void print(int i, int j) { // (int, int)
+    static private void print(int i, int j) { // (int, int)
         System.out.println("Two int values "+
                 i + " and " + j);
     }
--- src/test/java/de/hdm_stuttgart/mi/AppTest.java
+++ src/test/java/de/hdm_stuttgart/mi/AppTest.java
@@ -4,14 +4,48 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Unit test of {@link App}.
+ * Unit test of {@link Testing}.
  */
 public class AppTest {
-    /**
-     * Test for correct sum of two arguments.
-     */
+    // @Test drüber schreiben, testet auf Syntax Error
+    @Test
+    public void testCalculate(){
+
+        /* WICHTIGSTE VERGLEICHSMETHODEN:
+        1) assertEquals ODER assertNotEquals
+        2) assertTrue ODER  assertNotTrue
+
+        1) assertEquals ODER assertNotEquals
+        * Ich behaupte, dass ich wenn ich die Testing.calculate aufrufe mit 5,10 aufrufe,
+        * dass 15 rauskommen,
+        * sonst schmeiße Fehler */
+        Assert.assertEquals(10,Testing.calculate(5,10));
+        Assert.assertEquals(15,Testing.calculate(5,10));
+
+        // 2) assertTrue ODER  assertNotTrue
+        Assert.assertTrue(Testing.biggerTen(11));
+        Assert.assertFalse(Testing.biggerTen(5)); // IMMER AUCH AUF NEGATIV TESTEN
+    }
+
+    // Test for correct sum of two arguments.
     @Test
     public void testApp() {
-        Assert.assertEquals( 33, App.add(30, 3));
+        Assert.assertEquals( 33, Testing.add(30, 3));
+    }
+
+    // Test ob Primzahl ist
+    @Test
+    public void testIsPrime(){
+        Assert.assertTrue(Testing.isPrime(11));
+        Assert.assertTrue(Testing.isPrime(5));
+        Assert.assertTrue(Testing.isPrime(3));
+        Assert.assertTrue(Testing.isPrime(7));
+        Assert.assertTrue(Testing.isPrime(4));
+    }
+
+    // TEST FÜR DOUBLE-Werte
+    @Test
+    public void test(){
+        Assert.assertEquals(0,Testing.test(), 5E-15); // E verstehen!!!
     }
 }
