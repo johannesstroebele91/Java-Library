8ac3be41fdc7d6edfc0d0e0d30a6bdb0c8c02004
JohannesStroebele <stroebele.j@gmail.com>
2020-01-21 16:01:14 +0100

    Improved explanation of Call-By-Value, StringBuffer, and Scanner

--- src/main/java/de/hdm_stuttgart/mi/Input_Scanner.java
+++ src/main/java/de/hdm_stuttgart/mi/Input_Scanner.java
@@ -6,18 +6,21 @@ public class Input_Scanner {
     public static void main(String[] args) {
 
         // Creating an Object of the Scanner class
-        Scanner myObj = new Scanner(System.in); // can be used multiple times
+        Scanner scanner = new Scanner(System.in); // can be used multiple times
 
         // Declaring a String input with the value of the next line
         System.out.println("Enter dish:"); // UNNECESSARY: just advice for the input
-        String inputString = myObj.nextLine(); // IMPORTANT!
+        String inputString = scanner.nextLine(); // IMPORTANT!
         // inputString = myObj.next(); // next() only saves the next word (until whitespace)
         System.out.println("Dish is: " + inputString + "\n"); // UNNECESSARY: just printing the input
 
         // Declaring a int input with the value of the next int value
         System.out.println("Enter your favorite number:"); // UNNECESSARY: just advice for the input
-        int inputInt = myObj.nextInt();  // IMPORTANT!
+        int inputInt = scanner.nextInt();  // IMPORTANT!
         System.out.println("My favorite number: " + inputInt  + "\n"); // UNNECESSARY: just printing the input
+
+        // Scanner needs to be CLOSED at the end!!!
+        scanner.close();
 
         // Others input possibilities
         // double inputDouble = myObj.nextDouble();
