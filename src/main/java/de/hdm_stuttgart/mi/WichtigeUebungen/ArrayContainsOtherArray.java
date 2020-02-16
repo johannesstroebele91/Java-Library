f1e9736b94c495a1c55ae3aee55cd0af5365ed06
JohannesStroebele <stroebele.j@gmail.com>
2020-02-16 16:26:23 +0100

    Implemented explanation for function that examines if one array contains another one

---
+++ src/main/java/de/hdm_stuttgart/mi/WichtigeUebungen/ArrayContainsOtherArray.java
@@ -0,0 +1,27 @@
+package de.hdm_stuttgart.mi.WichtigeUebungen;
+
+import java.util.Arrays;
+
+public class ArrayContainsOtherArray {
+
+    public static void main(String[] args) {
+
+        int[] teil;
+        final int[] ganzes;
+        // Return true if one Array's elements are in another one
+        System.out.println( " Array is in another array contains(new int[]{1}, new int[]{1, 5, 6, 2}: " + contains(new int[]{1}, new int[]{1, 5, 6, 2}));
+        System.out.println( " Array is in another array contains(new int[]{1, 5}, new int[]{1, 5, 6, 2}: " + contains(new int[]{1, 5}, new int[]{1, 5, 6, 2}));
+
+        System.out.println( " Array is in another array new int[]{1, 5, 6, 2}, new int[]{5, 6}: " + contains(new int[]{1, 5, 6, 2}, new int[]{5, 6}));
+
+    }
+
+    static public boolean contains(final int[] teil, final int[] ganzes) {
+
+        for (int i = 0; i < ganzes.length - teil.length + 1; i++) {
+            if (Arrays.equals(teil, 0, teil.length, ganzes, i, teil.length + i))
+                return true;
+        }
+        return false;
+    }
+}
